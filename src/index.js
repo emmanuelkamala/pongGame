62166e3eaeb32a6b295356d4e06957d34869adcb
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-07-11 00:04:12 +0300

    Edit index.js file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import Phaser from 'phaser';
 import TitleScreen from './scenes/TitleScreen';
+import Game from './scenes/Game';
 
 const config = {
     width: 800,
     height: 600,
-    type: Phaser.AUTO
+    type: Phaser.AUTO,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200},
+            
+        }
+    }
 }
 
 const game = new Phaser.Game(config)
 
 game.scene.add('titlescreen', TitleScreen)
+game.scene.add('game', Game)
 
-game.scene.start('titlescreen')
+//game.scene.start('titlescreen')
+
+game.scene.start('game')
