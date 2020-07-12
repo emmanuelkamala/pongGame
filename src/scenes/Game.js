3bc6ec5190957987e0b46b07be9b9afb3f306637
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-07-13 00:09:33 +0300

    Create a paddle on the right part of the scene

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -7,31 +7,37 @@ class Game extends Phaser.Scene {
     }
 
     create(){
-       const ball = this.add.circle(200, 150, 10, 0xffffff, 1);
-       this.physics.add.existing(ball)
-       ball.body.setBounce(1, 1)
+       this.ball = this.add.circle(200, 150, 10, 0xffffff, 1);
+       this.physics.add.existing(this.ball)
+       this.ball.body.setBounce(1, 1)
 
-       ball.body.setCollideWorldBounds(true, 1, 1)
+       this.ball.body.setCollideWorldBounds(true, 1, 1)
 
-       ball.body.setVelocity(-200, 0)
+       this.ball.body.setVelocity(Phaser.Math.Between(-200, 200), Phaser.Math.Between(-200, 200))
 
        this.paddleLeft = this.add.rectangle(50, 250, 30, 100, 0xffffff, 1)
        this.physics.add.existing(this.paddleLeft, true)
 
+       this.paddleRight = this.add.rectangle(750, 250, 30, 100, 0xffffff, 1)
+       this.physics.add.existing(this.paddleRight, true)
+
        this.physics.add.collider(this.paddleLeft, ball)
+       this.physics.add.collider(this.paddleRight, ball)
 
        this.cursors = this.input.keyboard.createCursorKeys()
     }
 
     update(){
-        /** @type { Phaser.Physics.Arcade.Body } */
-        const body = this.paddleLeft.body;
+        /** @type { Phaser.Physics.Arcade.StaticBody } */
+        const body = this.paddleLeft.body
+
         if (this.cursors.up.isDown){
-            body.setVelocityY(-100);
-        } else if (this.cursors.down.isDown){
-            body.setVelocityY(100);
-        } else {
-            body.setVelocityY(0)
+            this.paddleLeft.y -= 10
+            body.updateFromGameObject()
+
+        } else if(this.cursors.down.isDown){
+            this.paddleLeft.y += 10
+            body.updateFromGameObject()
         }
     }
 }
