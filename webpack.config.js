dd2f507b2021cd2de5ab24a10b00045400863885
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-07-14 10:04:10 +0300

    Add github/workflow actions

--- webpack.config.js
+++ webpack.config.js
@@ -6,17 +6,6 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
     },
-    module: {
-        rules: [
-          {
-            test: /\.(mp3)$/i,
-            loader: 'file-loader',
-            options: {
-              outputPath: 'sounds',
-            },
-          },
-        ],
-      },
     devServer: {
         contentBase: './dist'
     }
